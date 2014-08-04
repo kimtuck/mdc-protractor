087c68a653747ff4a7ace3b7450318670eff5182
A477MZZ <Treasure2013>
2014-08-03 20:17:37 -0500

    base

--- dev/protractor/specs/login_spec.js
+++ dev/protractor/specs/login_spec.js
@@ -3,8 +3,8 @@ describe('User Can Login', function() {
         var user = {login: 'uat10mn@yahoo.com', pass: '3msuper#' };
         browser.get('http://cat.3m.com');
         browser.element(by.name('username')).sendKeys(user.login);
-        browser.element(by.name('password')).sendKeys(user.npmpass);
+        browser.element(by.name('password')).sendKeys(user.pass);
         browser.element(by.buttonText('Login')).click();
-        expect(browser.getCurrentUrl()).toBe('https://cat.3m.com/layout/index.html#/');
+        expect(browser.getCurrentUrl()).toBe('https://cat.3m.com/layout/index.html#/landing');
     })
 });
