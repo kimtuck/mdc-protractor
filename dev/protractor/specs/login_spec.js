7666fc1132831723afe81cd6316dbfe6ec92f418
A477MZZ <Treasure2013>
2014-08-03 20:28:32 -0500

    two

--- dev/protractor/specs/login_spec.js
+++ dev/protractor/specs/login_spec.js
@@ -1,10 +1,14 @@
 describe('User Can Login', function() {
     it('User can login',function() {
+        var username = browser.element(by.name('username'));
+        var password = browser.element(by.name('password'));
+        var button = browser.element(by.buttonText('Login'));
+
         var user = {login: 'uat10mn@yahoo.com', pass: '3msuper#' };
         browser.get('http://cat.3m.com');
-        browser.element(by.name('username')).sendKeys(user.login);
-        browser.element(by.name('password')).sendKeys(user.pass);
-        browser.element(by.buttonText('Login')).click();
+        username.sendKeys(user.login);
+        password.sendKeys(user.pass);
+        button.click();
         expect(browser.getCurrentUrl()).toBe('https://cat.3m.com/layout/index.html#/landing');
     })
 });
